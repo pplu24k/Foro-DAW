533862be2070d8c743d55a84e3dcdab27aa04096
pplu24k <joseluespp@gmail.com>
2023-02-23 18:32:57 +0100

    LOGIN y LOGUP funcionando

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  entrar(datos:any){
+
+    return this.httpClient.post('http://localhost:8000/api/auth/login',{
+
+      "email": datos.email,
+      "password": datos.pass,
+
+
+    })
+
+  }
 }
